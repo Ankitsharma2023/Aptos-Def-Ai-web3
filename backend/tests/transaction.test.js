7b2108a64a0b2540133ee7d154ccc33511a6b6dd
Raj Kishore S <127388780+Rajkishore08@users.noreply.github.com>
2025-03-24 16:40:14 +0530

    Update transaction.test.js

--- backend/tests/transaction.test.js
+++ backend/tests/transaction.test.js
@@ -1,1 +1,14 @@
+const request = require("supertest");
+const app = require("../index");
+
+describe("Transaction API Tests", () => {
+  it("should execute a transaction", async () => {
+    const res = await request(app)
+      .post("/api/transactions/execute")
+      .send({ amount: 10, type: "deposit" });
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("txHash");
+  });
+});
 
