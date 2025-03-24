660834dd96ea72117bbaf273c28d020c9f1e93c4
Raj Kishore S <127388780+Rajkishore08@users.noreply.github.com>
2025-03-24 16:33:07 +0530

    Update aptosClient.js

--- backend/src/config/aptosClient.js
+++ backend/src/config/aptosClient.js
@@ -1,1 +1,8 @@
+const { AptosClient } = require("aptos");
+
+const APTOS_NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.devnet.aptoslabs.com";
+
+const aptosClient = new AptosClient(APTOS_NODE_URL);
+
+module.exports = aptosClient;
 
