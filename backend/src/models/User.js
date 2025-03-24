5414c309842f7858c3fbf78d98ea5248a87ebe81
Raj Kishore S <127388780+Rajkishore08@users.noreply.github.com>
2025-03-24 16:33:22 +0530

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  walletAddress: String,
+  portfolio: { type: Array, default: [] },
+});
+
+module.exports = mongoose.model("User", UserSchema);
 
